122132c3578b6fd7b289c6ef10bfac953d4ea62f
Benoit Guillot <benoit.guillot@akretion.com.br>
2012-02-03 11:28:12 +0100

    [IMP] change views and add delivery_date in stock_move

--- stock_delivery_delays_working_days/stock.py
+++ stock_delivery_delays_working_days/stock.py
@@ -86,6 +86,34 @@ class stock_picking(osv.osv):
     _defautls = {
     }
 
+class stock_move(osv.osv):
+    _inherit = "stock.move"
 
+    def _get_move_from_delivery(self, cr, uid, ids, context=None):
+        res = self.pool.get('stock.move').search(cr, uid, [('picking_id.carrier_id', '=', ids[0]), ('state', '!=', 'done')], context=context)
+        return res
 
-stock_picking()
+    def _get_move_from_picking(self, cr, uid, ids, context=None):
+        res = self.pool.get('stock.move').search(cr, uid, [('picking_id.carrier_id', '=', ids[0]), ('state', '!=', 'done')], context=context)
+        return res
+
+    def _get_delivery_date(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        for move in self.browse(cr, uid, ids, context=context):
+            if not move.date_expected:
+                res[move.id] = False
+            elif not move.picking_id.carrier_id:
+                res[move.id] = move.date_expected
+            else:
+                start_date = datetime.strptime(move.date_expected, DEFAULT_SERVER_DATETIME_FORMAT)
+                res[move.id] = (self.pool.get('resource.calendar')._get_date(cr, uid, move.picking_id.carrier_id.calendar_id.id, start_date, move.picking_id.carrier_id.delivery_lead_time, context=context)).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        return res
+
+    _columns = {
+        'move_delivery_date': fields.function(_get_delivery_date, string='Delivery Date', type="datetime", help="Date of delivery to the customer",
+                                store= {
+                                'delivery.carrier':(_get_move_from_delivery, ['delivery_lead_time'], 10),
+                                'stock.picking':(_get_move_from_picking, ['carrier_id'], 10),
+                                'stock.move':(lambda self, cr, uid, ids, c=None: ids, ['date_planned'], 10),
+                                }),
+            }
