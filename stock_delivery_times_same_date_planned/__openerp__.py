774bc0c42f27b8f616a6e875eea5aa2d8a8f76f4
Benoit Guillot <benoit.guillot@akretion.com.br>
2014-01-16 16:01:59 +0100

    [IMP] clean code + port to v7.0

--- stock_delivery_times_same_date_planned/__openerp__.py
+++ stock_delivery_times_same_date_planned/__openerp__.py
@@ -1,24 +1,24 @@
-# -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-#    stock_delivery_times_same_date_planned for OpenERP                                          #
-#    Copyright (C) 2011 Akretion Benoît Guillot <benoit.guillot@akretion.com>   #
-#                                                                               #
-#    This program is free software: you can redistribute it and/or modify       #
-#    it under the terms of the GNU Affero General Public License as             #
-#    published by the Free Software Foundation, either version 3 of the         #
-#    License, or (at your option) any later version.                            #
-#                                                                               #
-#    This program is distributed in the hope that it will be useful,            #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-#    GNU Affero General Public License for more details.                        #
-#                                                                               #
-#    You should have received a copy of the GNU Affero General Public License   #
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
-#                                                                               #
-#################################################################################
-
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    stock_delivery_time_same_date_planned for OpenERP
+#    Copyright (C) 2011-2014 Akretion
+#    Author: Benoît Guillot <benoit.guillot@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 {
     'name': 'stock_delivery_times_same_date_planned',
@@ -28,12 +28,12 @@
     'description': """empty""",
     'author': 'Akretion',
     'website': 'http://www.akretion.com/',
-    'depends': ['sale','stock_delivery_times_working_days'], 
-    'init_xml': [],
-    'update_xml': [ 
-           'sale_view.xml',
-    ],
-    'demo_xml': [],
+    'depends': [
+        'sale',
+        'stock_delivery_times_working_days'
+        ],
+    'data': [],
+    'demo': [],
     'installable': True,
     'active': False,
 }
